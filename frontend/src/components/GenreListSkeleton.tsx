c768eef27edb3bc75dc69ae14f86d8c4438fd3ba
John John Penafiel <johnjohnpenafiel@hotmail.com>
2024-10-09 10:10:04 -0400

    update sidebar skeleton

--- frontend/src/components/GenreListSkeleton.tsx
+++ frontend/src/components/GenreListSkeleton.tsx
@@ -3,13 +3,22 @@ import {
   List,
   ListItem,
   Skeleton,
+  SkeletonCircle,
   SkeletonText,
 } from "@chakra-ui/react";
 
 const GenreListSkeleton = () => {
   return (
     <ListItem paddingY="10px">
-      <SkeletonText mt="3" noOfLines={1} spacing="2" skeletonHeight="2" />
+      <HStack>
+        <SkeletonCircle />
+        <SkeletonText
+          noOfLines={1}
+          spacing="2"
+          skeletonHeight="2"
+          width="100px"
+        />
+      </HStack>
     </ListItem>
   );
 };
