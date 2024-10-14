02b0183b39f6b8761195a08a074e9a1f6c62caa4
John John Penafiel <johnjohnpenafiel@hotmail.com>
2024-10-14 14:48:32 -0400

    build search input

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,13 +1,15 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/DALL·E Logo 2024-09-26.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 
 const NavBar = () => {
   return (
-    <HStack justifyContent="space-between" padding="10px">
+    <HStack padding="10px">
       <div style={{ padding: "5px" }}>
         <Image src={logo} boxSize="60px" borderRadius="10px" />
       </div>
+      <SearchInput />
       <ColorModeSwitch />
     </HStack>
   );
